e24437c4fd797bb717e711c60c1d2e4ba96d32f0
Niko Junnila <niko.junnila@goodlife.technology>
2025-12-18 16:04:31 +0200

    Update frontend and separate libs vendor chunk

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,13 +4,30 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 8082
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    server: {
+        port: 8082
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: manualChunks
+            }
+        }
     }
-  }
 })
+
+function manualChunks(id: any) {
+    if (id.includes('node_modules'))
+        return 'vendor';
+
+    if (id.includes('aire-typescript-sdk'))
+        return 'aire';
+
+    return null;
+}
